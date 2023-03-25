8effe6e1ff03bd60b5492b2ead9f3620c767da52
RonnySk <ronny_sk@hotmail.com>
2023-03-25 08:47:27 +0100

    app.js fix

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 
-app.use(express.static(__dirname + "public"));
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/html/home.html");
